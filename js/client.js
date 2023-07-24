109b2e2f9476a02ced17ecfd6c510008bf012337
Sonal <sonalschidrawar@gmail.com>
2023-07-24 22:45:41 +0530

    features added

--- js/client.js
+++ js/client.js
@@ -11,39 +11,92 @@ var audio = new Audio("ring.mp3");
 // function that appends event info to the container
 const append = (message, position) => {
   const messageElement = document.createElement("div");
+  const timestampElement = document.createElement("span"); // Create a new element for the timestamp
+  timestampElement.innerText = getCurrentTime();
+  timestampElement.classList.add("timestamp"); // Add a CSS class to the timestamp element
   messageElement.innerText = message;
   messageElement.classList.add("message");
   messageElement.classList.add(position);
+  messageElement.prepend(timestampElement); // Prepend the timestamp element to the message element
   messageContainer.append(messageElement);
   if (position == "left") {
     audio.play();
   }
 };
 
-// ask name anad let the server know
-const name = prompt("Enter your name to join");
-socket.emit("new-user-joined", name);
+// Function to get the current timestamp
+const getCurrentTime = () => {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const timestamp = `${hours}:${minutes}`;
+  return timestamp;
+};
 
-// if the new user joins, receive his/her name from the server
-socket.on("user-joined", (name) => {
-  append(`${name} joined the chat`, "right");
+// ask user Name  anad let the server know
+let userName = prompt("Enter your userName to join");
+while (!userName || userName.trim() === "") {
+  userName = prompt("Invalid userName. Please enter a valid userName to join");
+}
+socket.emit("new-user-joined", userName);
+
+// if the new user joins, receive their user Name  from the server
+socket.on("user-joined", (userName) => {
+  append(`${userName} joined the chat`, "right");
 });
 
-// if serverr sends a message, receive it
+// if server sends a message, receive it
 socket.on("receive", (data) => {
-  append(`${data.name}: ${data.message}`, "left");
+  const secretKey =
+    "66d77461b5ea7beb22e2b085331d619f11243a0306414fa837421d156c89af93";
+  console.log(data.message);
+  const decryptedMessage = CryptoJS.AES.decrypt(
+    data.message,
+    secretKey
+  ).toString(CryptoJS.enc.Utf8);
+  append(`${data.userName}: ${decryptedMessage}`, "left");
 });
 
 // if a user leaves the chat, append the info to the container
-socket.on("left", (name) => {
-  append(`${name} left`, "right");
+socket.on("left", (userName) => {
+  append(`${userName} left`, "right");
 });
+
+const chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
+
+// display chat history on joining
+chatHistory.forEach((message) => {
+  append(message, "left");
+});
+
+// function to delete the chat
+const deleteChat = () => {
+  messageContainer.innerHTML = ""; // Clear the chat container
+  localStorage.removeItem("chatHistory"); // Remove chat history from local storage
+};
+
+// listen for the delete chat button click event
+const deleteChatBtn = document.getElementById("bin");
+deleteChatBtn.addEventListener("click", deleteChat);
 
 // if form gets submitted, send server the message
 form.addEventListener("submit", (e) => {
   e.preventDefault(); //does not reload page
-  const message = messageInput.value;
-  append(`You: ${message}`, "right");
-  socket.emit("send", message);
-  messageInput.value = "";
+  const message = messageInput.value.trim();
+  if (message !== "") {
+    const secretKey =
+      "66d77461b5ea7beb22e2b085331d619f11243a0306414fa837421d156c89af93";
+    const encryptedMessage = CryptoJS.AES.encrypt(
+      message,
+      secretKey
+    ).toString();
+    append(`You: ${message}`, "right");
+    socket.emit("send", encryptedMessage);
+
+    // Store message in chat history in local storage
+    chatHistory.push(message);
+    localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+
+    messageInput.value = "";
+  }
 });
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,26 +5,42 @@ const io = require("socket.io")(8000, {
   },
 });
 const users = {};
+// CryptoJS library for encryption and decryption
+const CryptoJS = require("crypto-js");
+
+// Encryption function
+function encryptMessage(message, secretKey) {
+  return CryptoJS.AES.encrypt(message, secretKey).toString();
+}
+
+// Decryption function
+function decryptMessage(encryptedMessage, secretKey) {
+  const decryptedBytes = CryptoJS.AES.decrypt(encryptedMessage, secretKey);
+  return decryptedBytes.toString(CryptoJS.enc.Utf8);
+}
 
 io.on("connection", (socket) => {
   // if any new user joins, let other users connected to the server know
-  socket.on("new-user-joined", (name) => {
-    users[socket.id] = name;
-    socket.broadcast.emit("user-joined", name);
+  socket.on("new-user-joined", (userName) => {
+    users[socket.id] = userName;
+    socket.broadcast.emit("user-joined", userName);
   });
 
   // if someone sends a message, broadcast it to other people
-  socket.on("send", (message) => {
+  socket.on("send", (encryptedMessage) => {
+    const secretKey =
+      "66d77461b5ea7beb22e2b085331d619f11243a0306414fa837421d156c89af93";
+    const decryptedMessage = decryptMessage(encryptedMessage, secretKey);
     socket.broadcast.emit("receive", {
-      message: message,
-      name: users[socket.id],
+      message: encryptMessage(decryptedMessage, secretKey),
+      userName: users[socket.id],
     });
   });
 
   // if someone leaves the chat, let others know
-  socket.on("disconnect", (message) => {
+  socket.on("disconnect", () => {
     socket.broadcast.emit("left", users[socket.id]);
     delete users[socket.id];
-    //we are identifying the user by id becoz if users with same name enters in the chat, there must not be any issue.......identifying by id is more scalable
+    //we are identifying the user by id becoz if users with same userName  enters in the chat, there must not be any issue.......identifying by id is more scalable
   });
 });
